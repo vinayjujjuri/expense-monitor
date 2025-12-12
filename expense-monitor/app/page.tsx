4ee02ce160aaf70aeeb1f9404f1dd1da352cdd6a
vinayjujjuri <85351200+vinayjujjuri@users.noreply.github.com>
2025-12-12 11:29:40 +0530

    testing build success

--- expense-monitor/app/page.tsx
+++ expense-monitor/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Vinay Documentation
           </a>
         </div>
       </main>
